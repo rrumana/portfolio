4e5b52ffeb441162d0829a0b578556bdfe1da60c
rrumana <ryan.rumana@gmail.com>
2025-06-05 12:14:56 -0700

    Added lecture series and tweaked animations

--- static/javascript/scrollveal.js
+++ static/javascript/scrollveal.js
@@ -2,9 +2,11 @@ const defaultProps = {
   easing: "cubic-bezier(0.5, 0, 0, 1)",
   distance: "30px",
   origin: "bottom",
-  duration: 1000,
+  duration: 500,
   desktop: true,
   mobile: true,
+  viewFactor: 0.1, // Trigger animation when 20% of element is visible
+  viewOffset: { top: 0, bottom: -300 }, // Add offset for earlier triggering
 };
 // Hero Section
 ScrollReveal().reveal(".hero-title", {
@@ -20,44 +22,47 @@ ScrollReveal().reveal(".name", {
 });
 ScrollReveal().reveal(".hero-cta", {
   ...defaultProps,
-  delay: 1200,
+  delay: 700,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 ScrollReveal().reveal(".scroll-down-link", {
   ...defaultProps,
-  delay: 2000,
+  delay: 700,
   origin: "top",
 });
 
 /* About Section */
 ScrollReveal().reveal(".section-title", {
   ...defaultProps,
-  delay: 400,
+  delay: 100,
   distance: "0px",
+  viewOffset: { top: 0, bottom: -100 },
 });
 ScrollReveal().reveal(".about-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 200,
   scale: 0.5,
+  viewOffset: { top: 0, bottom: -100 },
 });
 
 ScrollReveal().reveal(".about-wrapper__info", {
   ...defaultProps,
-  delay: 700,
+  delay: 200,
   distance: "100px",
   origin: window.innerWidth > 768 ? "left" : "top",
+  viewOffset: { top: 0, bottom: -100 },
 });
 
 /* Projects Section */
 ScrollReveal().reveal(".project-wrapper__text", {
   ...defaultProps,
-  delay: 600,
+  delay: 300,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 
 ScrollReveal().reveal(".project-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 300,
   scale: 0.9,
   origin: window.innerWidth > 768 ? "right" : "bottom",
 });
